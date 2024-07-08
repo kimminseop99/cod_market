5b2ca0c3d51bc76cf41e212f209b4cab04fae97f
kimminseop99 <minseopkim99@gmail.com>
2024-07-08 16:14:17 +0900

    main UI/UX

--- src/main/java/com/cod/market/home/HomeController.java
+++ src/main/java/com/cod/market/home/HomeController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "홈 화면 입니다.";
+        return "main";
     }
 }
