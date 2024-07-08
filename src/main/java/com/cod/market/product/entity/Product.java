a994c63a54ca3aa681c85ae92fc840f7d7e5d676
kimminseop99 <minseopkim99@gmail.com>
2024-07-08 16:46:12 +0900

    product list

--- src/main/java/com/cod/market/product/entity/Product.java
+++ src/main/java/com/cod/market/product/entity/Product.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
