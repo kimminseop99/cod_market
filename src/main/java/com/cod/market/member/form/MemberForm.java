d768de11c73630e812a100d8cabaef2df4401a6a
kimminseop99 <minseopkim99@gmail.com>
2024-07-09 16:17:20 +0900

    join form

---
+++ src/main/java/com/cod/market/member/form/MemberForm.java
@@ -0,0 +1,32 @@
+package com.cod.market.member.form;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+public class MemberForm {
+
+    @Size(min = 3, max = 25)
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String nickname;
+
+    @Email
+    @NotBlank
+    private String email;
+
+}
