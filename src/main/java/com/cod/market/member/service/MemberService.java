26ec3d5c30137bb52eeeb89d4dec66375f8b0223
kimminseop99 <minseopkim99@gmail.com>
2024-07-09 14:42:23 +0900

    member test

--- src/main/java/com/cod/market/member/service/MemberService.java
+++ src/main/java/com/cod/market/member/service/MemberService.java
@@ -1,7 +1,12 @@
 package com.cod.market.member.service;
 
+import com.cod.market.member.entity.Member;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+
+    public void join(String username, String password, String email, String nickname) {
+
+    }
 }
