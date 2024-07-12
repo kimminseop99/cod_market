dbb755a0d2f28673c9b358e49dea85df91a4738c
kimminseop99 <minseopkim99@gmail.com>
2024-07-12 14:40:45 +0900

    ãadd cart entity, service, repo & edit detail page

---
+++ src/main/java/com/cod/market/cart/entity/Cart.java
@@ -0,0 +1,12 @@
+package com.cod.market.cart.entity;
+
+import com.cod.market.base.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Cart extends BaseEntity {
+}
---
+++ src/main/java/com/cod/market/cart/service/CartService.java
@@ -0,0 +1,10 @@
+package com.cod.market.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+}
