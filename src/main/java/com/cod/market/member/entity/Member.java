56d8d506cdfcf133802b853da5c3b7989faf6c8b
kimminseop99 <minseopkim99@gmail.com>
2024-07-12 15:05:45 +0900

    cart entity config

--- src/main/java/com/cod/market/member/entity/Member.java
+++ src/main/java/com/cod/market/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.cod.market.member.entity;
 
 import com.cod.market.base.BaseEntity;
+import com.cod.market.cart.entity.Cart;
 import com.cod.market.question.entity.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -25,4 +26,7 @@ public class Member extends BaseEntity {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Question> questionList;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Cart> cartList;
 }
