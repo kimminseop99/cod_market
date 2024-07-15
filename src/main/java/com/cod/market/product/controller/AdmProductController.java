a62437b7bf3ddbe6c6f1e8594fd2c5cfa392987f
kimminseop99 <minseopkim99@gmail.com>
2024-07-15 15:09:27 +0900

    add admin product content

--- src/main/java/com/cod/market/product/controller/AdmProductController.java
+++ src/main/java/com/cod/market/product/controller/AdmProductController.java
@@ -4,7 +4,9 @@ import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +19,11 @@ public class AdmProductController {
         return "adm/product/create";
     }
 
+    @PostMapping("/create")
+    public String createContent(@RequestParam("name") String name,@RequestParam("description") String description,  @RequestParam("price") int price ){
+        productService.create(name, description, price);
+
+        return "adm/product/create";
+    }
 
 }
