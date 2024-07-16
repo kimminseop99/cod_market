e3f588d4c952f978635cef9492d7de7d345e200b
kimminseop99 <minseopkim99@gmail.com>
2024-07-16 15:12:16 +0900

    add product devInitData

--- src/main/java/com/cod/market/base/initData/DevInitData.java
+++ src/main/java/com/cod/market/base/initData/DevInitData.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.UUID;
+
 @Configuration
 @Profile("dev")
 public class DevInitData implements BeforeInitData{
@@ -22,6 +24,12 @@ public class DevInitData implements BeforeInitData{
             memberService.join("user2", password, "user2@test.com", "user2");
             memberService.join("user3", password, "user3@test.com", "user3");
             memberService.join("user4", password, "user4@test.com", "user4");
+
+
+            productService.create("제목1", "설명1 입니다.", 10000);
+            productService.create("제목2", "설명2 입니다.", 20000);
+            productService.create("제목3", "설명3 입니다.", 30000);
+            productService.create("제목4", "설명4 입니다.", 40000);
         };
     }
 }
