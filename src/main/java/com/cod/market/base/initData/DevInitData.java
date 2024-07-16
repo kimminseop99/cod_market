0f9170bdba6e0ed2a0370f78139e34ea5e32983d
kimminseop99 <minseopkim99@gmail.com>
2024-07-16 14:39:37 +0900

    add membrt devInitdata

--- src/main/java/com/cod/market/base/initData/DevInitData.java
+++ src/main/java/com/cod/market/base/initData/DevInitData.java
@@ -1,5 +1,8 @@
 package com.cod.market.base.initData;
 
+import com.cod.market.member.entity.Member;
+import com.cod.market.member.service.MemberService;
+import com.cod.market.product.service.ProductService;
 import groovyjarjarpicocli.CommandLine;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +13,15 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements BeforeInitData{
     @Bean
-    CommandLineRunner initData(){
+    CommandLineRunner initData(MemberService memberService, ProductService productService){
         return args -> {
             beforeInit();
+
+            String password = "{noop}1234";
+            memberService.join("user1", password, "user1@test.com", "user1");
+            memberService.join("user2", password, "user2@test.com", "user2");
+            memberService.join("user3", password, "user3@test.com", "user3");
+            memberService.join("user4", password, "user4@test.com", "user4");
         };
     }
 }
